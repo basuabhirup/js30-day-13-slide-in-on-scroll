151f5bcf1ee403e38e324d15411f12a940008d62
basuabhirup <basu.abhirup@yahoo.com>
2021-08-14 22:08:20 +0530

    Set slide in on scroll.

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide(e) {
+  for (let i = 0; i < sliderImages.length; i++) {
+    // half way through the image:
+    const slideInAt = (window.scrollY + window.innerHeight - sliderImages[i].height / 2);
+    // bottom of the image:
+    const imageBottom = sliderImages[i].offsetTop + sliderImages[i].height;
+    // conditional statements:
+    const isHalfShown = slideInAt > sliderImages[i].offsetTop;
+    const scrolledPast = window.scrollY > imageBottom;
+
+    if (isHalfShown && !scrolledPast) {
+      sliderImages[i].classList.add("active");
+    } else {
+      sliderImages[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
